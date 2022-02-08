02c66aa6e1109a5b0792e7ceb021c23ccdf5843a
Mazahir <Supermazahir@gmail.com>
2022-02-08 18:18:11 +0530

    Updated files

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,34 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import nodemailer from "nodemailer";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  if (req.method === "GET") {
+    const transporter = nodemailer.createTransport({
+      service: "Gmail", // no need to set host or port etc.
+      auth: {
+        user: "bejokerr26@gmail.com",
+        pass: "23390247",
+      },
+    });
+    try {
+      const data = await transporter.sendMail({
+        to: "supermazahir@gmail.com",
+        from: "bejokerr26@gmail.com",
+        subject: "Signup verification",
+        html: '<h1>Please verify your email</h1><a href="www.google.com">',
+      });
+      console.log("Email can be sent", data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  res.status(200).json({ name: `John Doe` });
 }
