f50799f608ed552ca93808583611290d4b713d1c
Mazahir <Supermazahir@gmail.com>
2022-02-09 20:27:29 +0530

    Updated files

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,4 +1,5 @@
 import { Box, Flex, Heading, Text, useColorMode } from "@chakra-ui/react";
+import moment from "moment";
 
 export default function Card({
   color,
@@ -14,7 +15,6 @@ export default function Card({
   newDate: boolean;
 }) {
   const { colorMode } = useColorMode();
-
   return (
     <Flex
       alignItems={"center"}
@@ -23,12 +23,24 @@ export default function Card({
       mx="2"
     >
       {newDate ? (
-        <Heading size={"lg"} mr="2" w={["14", "16", "20"]}>
-          {date.format("Do")}
-        </Heading>
-      ) : (
-        <Box mr="2" w={["14", "16", "20"]} />
-      )}
+        <Flex
+          justifyContent={"space-between"}
+          alignItems="center"
+          alignContent={"center"}
+          flexDirection="column"
+          w={["14", "16", "20"]}
+        >
+          <Heading size={"lg"} mr="2">
+            {date.format("Do")}
+          </Heading>
+          {moment(date).set("year", currentDate.year()).format("L") ===
+          moment().format("L") ? (
+            <Box p="2" borderRadius={"md"} py="0.5" my="1" bg="brand.100">
+              <Text color={"white"}>Today</Text>
+            </Box>
+          ) : null}
+        </Flex>
+      ) : null}
       <Box
         flex={1}
         as="button"
@@ -64,10 +76,17 @@ export default function Card({
             h="full"
             alignItems="flex-start"
           >
-            <Heading isTruncated size={"md"}>
+            <Heading
+              isTruncated
+              color={colorMode == "dark" ? "white" : getContrast(color)}
+              size={"md"}
+            >
               {name}'s Birthday
             </Heading>
-            <Text size={"md"}>
+            <Text
+              color={colorMode == "dark" ? "white" : getContrast(color)}
+              size={"md"}
+            >
               {Math.abs(date.diff(currentDate, "years"))} years old
             </Text>
           </Flex>
@@ -75,4 +94,22 @@ export default function Card({
       </Box>
     </Flex>
   );
+}
+
+function getContrast(hexcolor: string) {
+  if (hexcolor.slice(0, 1) === "#") {
+    hexcolor = hexcolor.slice(1);
+  }
+  var aRgbHex = hexcolor.match(/.{1,2}/g);
+  if (aRgbHex) {
+    var Rgb = [
+      parseInt(aRgbHex[0], 16),
+      parseInt(aRgbHex[1], 16),
+      parseInt(aRgbHex[2], 16),
+    ];
+    const brightness = Math.round(
+      (Rgb[0] * 299 + Rgb[1] * 587 + Rgb[2] * 114) / 1000
+    );
+    return brightness > 125 ? "black" : "white";
+  } else return "black";
 }
