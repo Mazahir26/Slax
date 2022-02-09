721b135643501db1feb72cbc2bfb15755ce721b4
Mazahir <Supermazahir@gmail.com>
2022-02-09 20:27:29 +0530

    Updated files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,56 +1,20 @@
-import { AddIcon, ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
-import {
-  Flex,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
-  useDisclosure,
-  Button,
-  FormControl,
-  FormLabel,
-  Input,
-  Box,
-  Heading,
-  IconButton,
-  useColorMode,
-} from "@chakra-ui/react";
+import { Flex, useDisclosure, useColorMode } from "@chakra-ui/react";
 import moment from "moment";
 import type { NextPage } from "next";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import List from "../components/Calender";
-// const events = [
-//   {
-//     id: 1,
-//     startAt: "2021-11-21T18:00:00.000Z",
-//     endAt: "2021-11-21T19:00:00.000Z",
-//     summary: "test",
-//     color: "blue",
-//     calendarID: "work",
-//   },
-//   {
-//     id: 2,
-//     startAt: "2021-11-21T18:00:00.000Z",
-//     endAt: "2021-11-21T19:00:00.000Z",
-//     summary: "test",
-//     color: "blue",
-//   },
-// ];
-
+import TopBar from "../components/TopBar";
+import AddBirthdayModal from "../components/Modal";
 type event = {
   name: string;
   date: moment.Moment;
 };
 
 const Home: NextPage = (props) => {
-  const [events, setEvents] = useState<event[]>();
+  const [events, setEvents] = useState<event[]>([]);
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [currentYear, setCurrentYear] = useState(moment());
   const { colorMode } = useColorMode();
-
   return (
     <>
       <Flex
@@ -60,87 +24,14 @@ const Home: NextPage = (props) => {
         width={"full"}
         h={"90vh"}
       >
-        <Box
-          alignSelf={"stretch"}
-          alignContent={"center"}
-          borderRadius="lg"
-          boxShadow="md"
-          bg={colorMode == "dark" ? "gray.600" : "white"}
-          p={"4"}
-          m="2"
-        >
-          <Flex flexDirection={"row"} justifyContent="space-between">
-            <Flex flexDirection={"row"} justifyContent="flex-start">
-              <IconButton
-                variant={"ghost"}
-                aria-label="back"
-                icon={<ChevronLeftIcon w={6} h={6} />}
-                onClick={() =>
-                  setCurrentYear(moment(currentYear).subtract(1, "year"))
-                }
-              />
-              <Heading mx="2" size={"lg"} fontWeight="semibold">
-                {currentYear.format("YYYY")}
-              </Heading>
-              <IconButton
-                variant={"ghost"}
-                aria-label="back"
-                icon={<ChevronRightIcon w={6} h={6} />}
-                onClick={() =>
-                  setCurrentYear(moment(currentYear).add(1, "year"))
-                }
-              />
-            </Flex>
-            <Button
-              borderRadius={"3xl"}
-              leftIcon={<AddIcon />}
-              colorScheme={"brand"}
-              variant="outline"
-              onClick={() => onOpen()}
-            >
-              Add Birthday
-            </Button>
-          </Flex>
-        </Box>
-        <List
-          year={currentYear}
-          events={[
-            {
-              name: "person 1",
-              date: moment("2002-03-23"),
-            },
-            {
-              name: "person 2",
-              date: moment("2002-03-24"),
-            },
-            {
-              name: "person 1",
-              date: moment("2003-03-24"),
-            },
-          ]}
+        <TopBar
+          currentYear={currentYear}
+          onOpen={onOpen}
+          setCurrentYear={setCurrentYear}
         />
+        <List year={currentYear} events={events} />
       </Flex>
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Add a Birthday</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <FormControl>
-              <FormLabel>Name</FormLabel>
-              <Input placeholder="eg. Saitama" />
-            </FormControl>
-          </ModalBody>
-          <ModalFooter>
-            <Button color={"#5E7CE2"} mr={3} onClick={onClose}>
-              Save
-            </Button>
-            <Button onClick={onClose} variant="ghost">
-              Close
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+      <AddBirthdayModal isOpen={isOpen} onClose={onClose} />
     </>
   );
 };
