6331771ab75c20074be55cd3e1df307561ef704d
Mazahir <Supermazahir@gmail.com>
2022-02-12 21:17:23 +0530

    Updated Files

--- pages/api/createEvent.ts
+++ pages/api/createEvent.ts
@@ -1,49 +1,62 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
+import { json } from "stream/consumers";
 import { rawEvent } from "../../components/types";
 import client from "../../lib/mongodb";
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const session = await getSession({ req });
-  if (session?.user?.email)
-    return res.status(401).json({
-      msg: "You need to be authenticated",
-      code: 401,
-    });
-  if (req.method === "POST") {
-    if (req.body.name || req.body.date) {
-      return res.status(400).json({
-        msg: "Bad Request",
-        code: 400,
-      });
-    }
-    try {
-      const cli = await client;
-      const database = cli.db("Data");
-      const reminders = database.collection<rawEvent>("reminders");
-      if (!session?.user?.email)
-        return res.status(401).json({
-          msg: "You need to be authenticated",
-          code: 401,
-        });
-      const result = await reminders.insertOne({
-        date: new Date(),
-        name: req.body.name,
-        user: session?.user?.email,
-      });
-      return res.status(201).json(result);
-    } catch (e) {
-      return res.status(500).json({
-        msg: "Ops something went wrong",
-        code: 500,
-      });
-    }
-  } else {
-    return res.status(400).json({
-      msg: "Get is not allowed in this Api route",
-      code: 400,
-    });
+  try {
+    const session = await fetch("http://localhost:3000/api/auth/session").then(
+      (res) => res.json()
+    );
+    console.log(session);
+  } catch (e) {
+    console.warn(e);
   }
+  return res.status(400).json({
+    msg: "Get is not allowed in this Api route",
+    code: 400,
+  });
+  // if (session?.user?.email)
+  //   return res.status(401).json({
+  //     msg: "You need to be authenticated",
+  //     code: 401,
+  //   });
+  // if (req.method === "POST") {
+  //   if (!req.body.name || !req.body.date) {
+  //     console.log(req.body.name, req.body.date);
+  //     return res.status(400).json({
+  //       msg: "Bad Request",
+  //       code: 400,
+  //     });
+  //   }
+  //   try {
+  //     const cli = await client;
+  //     const database = cli.db("Data");
+  //     const reminders = database.collection<rawEvent>("reminders");
+  //     if (!session?.user?.email)
+  //       return res.status(401).json({
+  //         msg: "You need to be authenticated",
+  //         code: 401,
+  //       });
+  //     const result = await reminders.insertOne({
+  //       date: new Date(),
+  //       name: req.body.name,
+  //       user: session?.user?.email,
+  //     });
+  //     return res.status(201).json(result);
+  //   } catch (e) {
+  //     return res.status(500).json({
+  //       msg: "Ops something went wrong",
+  //       code: 500,
+  //     });
+  //   }
+  // } else {
+  //   return res.status(400).json({
+  //     msg: "Get is not allowed in this Api route",
+  //     code: 400,
+  //   });
+  // }
 }
