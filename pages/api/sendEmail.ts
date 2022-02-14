9526e147ad7e660d2a63acbbbc8d43a6880b9249
Mazahir <Supermazahir@gmail.com>
2022-02-14 18:09:44 +0530

    Updated Files

--- pages/api/sendEmail.ts
+++ pages/api/sendEmail.ts
@@ -13,14 +13,14 @@ export default async function handler(
     const transporter = nodemailer.createTransport({
       service: "Gmail", // no need to set host or port etc.
       auth: {
-        user: "***REMOVED***",
-        pass: "***REMOVED***",
+        user: "bejokerr26@gmail.com",
+        pass: "23390247",
       },
     });
     try {
       const data = await transporter.sendMail({
         to: "supermazahir@gmail.com",
-        from: "***REMOVED***",
+        from: "bejokerr26@gmail.com",
         subject: "Signup verification",
         html: '<h1>Please verify your email</h1><a href="www.google.com">',
       });
