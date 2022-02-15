d28123109f63c7225cb27a64461f2b50495950d9
Mazahir <Supermazahir@gmail.com>
2022-02-15 11:31:14 +0530

    Updated files

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -3,25 +3,23 @@ import { MongoClient } from "mongodb";
 const uri = process.env.MONGODB_URI;
 
 let client;
-let clientPromise;
 if (!uri) {
   throw new Error("Please add your Mongo URI to .env.local");
 }
-if (process.env.NODE_ENV === "development") {
-  //@ts-ignore
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri);
-    //@ts-ignore
-    global._mongoClientPromise = client.connect();
-  }
-  //@ts-ignore
-  clientPromise = global._mongoClientPromise;
-} else {
-  client = new MongoClient(uri);
-  clientPromise = client.connect();
-}
+// if (process.env.NODE_ENV === "development") {
+//   //@ts-ignore
+//   if (!global._mongoClientPromise) {
+//     client = new MongoClient(uri);
+//     //@ts-ignore
+//     global._mongoClientPromise = client.connect();
+//   }
+//   //@ts-ignore
+//   clientPromise = global._mongoClientPromise;
+// } else {
+//   client = new MongoClient(uri);
+//   clientPromise = client.connect();
+// }
 
 client = new MongoClient(uri);
-clientPromise = client.connect();
 
-export default <Promise<MongoClient>>clientPromise;
+export default <MongoClient>client;
