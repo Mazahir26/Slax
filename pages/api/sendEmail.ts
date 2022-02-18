b8945b840cd61ddf4bbbd9e18c4e1a7f5e09c48a
Mazahir <Supermazahir@gmail.com>
2022-02-18 18:36:49 +0530

    Send mails

--- pages/api/sendEmail.ts
+++ pages/api/sendEmail.ts
@@ -8,7 +8,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  res.status(200).json({
+  return res.status(200).json({
     mgs: req.body.key,
   });
   const curTime = parseInt(moment().format("kkmm"));
