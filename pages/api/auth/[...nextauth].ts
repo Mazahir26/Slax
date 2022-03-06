39c232111be66f015626c8b333c5dbcb1cb951eb
Mazahir <Supermazahir@gmail.com>
2022-03-06 11:42:39 +0530

    Test deploy

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -41,6 +41,13 @@ export default NextAuth({
           if (!access_token.token) {
             throw new Error("Error while Sending Mail");
           }
+          console.log({
+            user: process.env.EMAIL_SERVER_USER,
+            clientId: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+            refreshToken: process.env.REFRESH_TOKEN,
+            accessToken: access_token.token,
+          });
           const transport = nodemailer.createTransport({
             service: "gmail",
             auth: {
