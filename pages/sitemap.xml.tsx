54b9aaddbeab39a4da5bfb6d61a314cc1d91b61f
Mazahir <Supermazahir@gmail.com>
2022-03-15 13:06:08 +0530

    Updated sitemap

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -10,19 +10,20 @@ export const getServerSideProps = ({ res, req }: GetServerSidePropsContext) => {
       ? `http://${req.headers.host}`
       : `https://${req.headers.host}`;
 
-  const staticPages = fs
-    .readdirSync("pages")
-    .filter((staticPage) => {
-      return ![
-        "_app.js",
-        "_document.js",
-        "_error.js",
-        "sitemap.xml.js",
-      ].includes(staticPage);
-    })
-    .map((staticPagePath) => {
-      return `${baseUrl}/${staticPagePath}`;
-    });
+  const staticPages = [
+    "404",
+    "about",
+    "dashboard",
+    "error",
+    "index",
+    "login",
+    "signout",
+    "signup",
+    "sitemap.xml",
+  ].map((staticPagePath) => {
+    return `${baseUrl}/${staticPagePath}`;
+  });
+
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     ${staticPages
@@ -31,7 +32,7 @@ export const getServerSideProps = ({ res, req }: GetServerSidePropsContext) => {
           <url>
             <loc>${url}</loc>
             <lastmod>${new Date().toISOString()}</lastmod>
-            <changefreq>monthly</changefreq>
+            <changefreq>yearly</changefreq>
             <priority>1.0</priority>
           </url>
         `;
